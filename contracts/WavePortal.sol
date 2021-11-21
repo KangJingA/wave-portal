322f82958b06e8f61c907607cdca2bc5fe5a1907
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-11-21 15:43:05 +0800

    add wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    // state variable that stored in permanently in the local storage
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    // msg.sender -> address of person who has called the function
+    // public makes the function available on the blockchain
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,27 +1,38 @@
 // HRE = hardhat runtime environment
 // no need to import it
 const main = async () => {
-    // compile contract and generate the necessary files we need to work with our contract 
-    // under the artifacts directory
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+	// grab owner and randomPerson wallet address
+	const [owner, randomPerson] = await hre.ethers.getSigners()
 
-    // create local blockchain network for this contract 
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+	// compile contract and generate the necessary files we need to work with our contract
+	// under the artifacts directory
+	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
 
+	// create local blockchain network for this contract
+	const waveContract = await waveContractFactory.deploy()
+	await waveContract.deployed()
+	console.log("Contract deployed to:", waveContract.address)
+	console.log("Contract deployed by:", owner.address)
+
+	let waveCount
+	waveCount = await waveContract.getTotalWaves()
+
+	let waveTxn = await waveContract.wave()
+	await waveTxn.wait()
+
+	waveCount = await waveContract.getTotalWaves()
+}
+
+const runMain = async () => {
+	try {
+		await main()
+		process.exit(0)
+	} catch (error) {
+		console.log(error)
+		process.exit(1)
+	}
+}
+
+runMain()
 
 // npx hardhat run scripts/run.js
